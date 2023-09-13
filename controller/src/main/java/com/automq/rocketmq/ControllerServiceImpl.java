74188ba5cfc36c78949063eb22284de1556ff2d0
Li Zhanhui <lizhanhui@gmail.com>
2023-09-13 15:19:15 +0800

    fix: code style issues
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

--- controller/src/main/java/com/automq/rocketmq/ControllerServiceImpl.java
+++ controller/src/main/java/com/automq/rocketmq/ControllerServiceImpl.java
@@ -1,6 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.automq.rocketmq;
 
-import apache.rocketmq.controller.v1.*;
+import apache.rocketmq.controller.v1.AddOssSegmentReply;
+import apache.rocketmq.controller.v1.AddOssSegmentRequest;
+import apache.rocketmq.controller.v1.BrokerHeartbeatReply;
+import apache.rocketmq.controller.v1.BrokerHeartbeatRequest;
+import apache.rocketmq.controller.v1.BrokerRegistrationReply;
+import apache.rocketmq.controller.v1.BrokerRegistrationRequest;
+import apache.rocketmq.controller.v1.BrokerUnregistrationReply;
+import apache.rocketmq.controller.v1.BrokerUnregistrationRequest;
+import apache.rocketmq.controller.v1.Code;
+import apache.rocketmq.controller.v1.CommitOffsetReply;
+import apache.rocketmq.controller.v1.CommitOffsetRequest;
+import apache.rocketmq.controller.v1.ControllerServiceGrpc;
+import apache.rocketmq.controller.v1.CreateTopicReply;
+import apache.rocketmq.controller.v1.CreateTopicRequest;
+import apache.rocketmq.controller.v1.DeleteOssSegmentReply;
+import apache.rocketmq.controller.v1.DeleteOssSegmentRequest;
+import apache.rocketmq.controller.v1.DeleteTopicReply;
+import apache.rocketmq.controller.v1.DeleteTopicRequest;
+import apache.rocketmq.controller.v1.DescribeTopicReply;
+import apache.rocketmq.controller.v1.DescribeTopicRequest;
+import apache.rocketmq.controller.v1.ListMessageQueueReassignmentsReply;
+import apache.rocketmq.controller.v1.ListMessageQueueReassignmentsRequest;
+import apache.rocketmq.controller.v1.ListOssSegmentsRequest;
+import apache.rocketmq.controller.v1.ListOssSegmentsResponse;
+import apache.rocketmq.controller.v1.ListTopicMessageQueueAssignmentsReply;
+import apache.rocketmq.controller.v1.ListTopicMessageQueueAssignmentsRequest;
+import apache.rocketmq.controller.v1.ListTopicsReply;
+import apache.rocketmq.controller.v1.ListTopicsRequest;
+import apache.rocketmq.controller.v1.ReassignMessageQueueReply;
+import apache.rocketmq.controller.v1.ReassignMessageQueueRequest;
+import apache.rocketmq.controller.v1.Status;
+import apache.rocketmq.controller.v1.Topic;
+import apache.rocketmq.controller.v1.UpdateTopicRequest;
 import com.google.protobuf.TextFormat;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
