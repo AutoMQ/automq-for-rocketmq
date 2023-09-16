c6557278cc7503dee37a78461ad9efe4da4c0d4a
SSpirits <admin@lv5.moe>
2023-09-16 19:50:59 +0800

    fix(store): make checkstyle happy
    Signed-off-by: SSpirits <admin@lv5.moe>

--- store/src/main/java/com/automq/rocketmq/store/impl/MessageStoreImpl.java
+++ store/src/main/java/com/automq/rocketmq/store/impl/MessageStoreImpl.java
@@ -36,7 +36,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicLong;
 import org.rocksdb.RocksDBException;
 
 import static com.automq.rocketmq.store.util.SerializeUtil.buildCheckPointKey;
