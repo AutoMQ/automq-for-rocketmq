14d1954cec1b677483b501fde1b68bc16a32b39f
Li Zhanhui <lizhanhui@gmail.com>
2023-09-21 11:41:02 +0800

    feat: support queue assignment CRUD
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

---
+++ controller/src/main/java/com/automq/rocketmq/controller/metadata/database/dao/QueueAssignment.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.automq.rocketmq.controller.metadata.database.dao;
+
+import java.util.Date;
+
+public class QueueAssignment {
+    private int topicId;
+
+    private int queueId;
+
+    private int srcNodeId;
+
+    private int dstNodeId;
+
+    private QueueAssignmentStatus status;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    public int getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(int topicId) {
+        this.topicId = topicId;
+    }
+
+    public int getQueueId() {
+        return queueId;
+    }
+
+    public void setQueueId(int queueId) {
+        this.queueId = queueId;
+    }
+
+    public int getSrcNodeId() {
+        return srcNodeId;
+    }
+
+    public void setSrcNodeId(int srcNodeId) {
+        this.srcNodeId = srcNodeId;
+    }
+
+    public int getDstNodeId() {
+        return dstNodeId;
+    }
+
+    public void setDstNodeId(int dstNodeId) {
+        this.dstNodeId = dstNodeId;
+    }
+
+    public QueueAssignmentStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(QueueAssignmentStatus status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
