6ec90120681163bde13a70474f53cf5a554c5283
Li Zhanhui <lizhanhui@gmail.com>
2023-09-21 10:58:52 +0800

    feat: support CRUD for topic
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

---
+++ controller/src/main/java/com/automq/rocketmq/controller/metadata/database/dao/Topic.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.automq.rocketmq.controller.metadata.database.dao;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Topic {
+    private int id;
+    private String name;
+    private int queueNum;
+    private TopicStatus status = TopicStatus.ACTIVE;
+    private Date createTime;
+    private Date updateTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQueueNum() {
+        return queueNum;
+    }
+
+    public void setQueueNum(int queueNum) {
+        this.queueNum = queueNum;
+    }
+
+    public TopicStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TopicStatus status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Topic topic = (Topic) o;
+        return id == topic.id && queueNum == topic.queueNum && name.equals(topic.name) && status == topic.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, queueNum, status);
+    }
+}
