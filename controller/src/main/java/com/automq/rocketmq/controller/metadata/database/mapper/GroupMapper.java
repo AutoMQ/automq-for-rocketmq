53885543aa6c5a36c303dbcb75e3b0e2a481e277
Zhanhui Li <lizhanhui@gmail.com>
2023-09-22 14:38:17 +0800

    feat: setup mapper for ConsumerGroup, ConsumerGroupProgress, Queue, S… (#79)
    Signed-off-by: Li Zhanhui <lizhanhui@gmail.com>

---
+++ controller/src/main/java/com/automq/rocketmq/controller/metadata/database/mapper/GroupMapper.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.automq.rocketmq.controller.metadata.database.mapper;
+
+import com.automq.rocketmq.controller.metadata.database.dao.Group;
+import com.automq.rocketmq.controller.metadata.database.dao.GroupStatus;
+import java.util.Date;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface GroupMapper {
+
+    int create(Group group);
+
+    /**
+     * If <code>null != id</code>, the exact row will be returned; otherwise, all existing records will be
+     *
+     * @param id         Optionally group-id
+     * @param status     Optional group status
+     * @param updateTime Optionally update-time
+     * @return
+     */
+    List<Group> list(@Param("id") Long id, @Param("status") GroupStatus status, @Param("updateTime") Date updateTime);
+
+    /**
+     * Only group name can be modified.
+     *
+     * @param group
+     * @return
+     */
+    int update(Group group);
+
+    /**
+     * Delete consumer group records from table.
+     * <strong>Warning</strong>
+     * If <code>id</code> is null, all records will be deleted.
+     *
+     * @param id Optional group-id to delete
+     * @return Number of rows affected.
+     */
+    int delete(Long id);
+}
--- controller/src/main/resources/ddl.sql
+++ controller/src/main/resources/ddl.sql
@@ -14,114 +14,141 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-CREATE TABLE IF NOT EXISTS lease (
-    node_id INT NOT NULL ,
-    epoch INT NOT NULL DEFAULT 0,
+CREATE TABLE IF NOT EXISTS lease
+(
+    node_id         INT      NOT NULL,
+    epoch           INT      NOT NULL DEFAULT 0,
     expiration_time DATETIME NOT NULL
 );
 
-INSERT INTO lease(node_id, epoch, expiration_time) VALUES (0, 0, TIMESTAMP('2023-01-01'));
+INSERT INTO lease(node_id, epoch, expiration_time)
+VALUES (0, 0, TIMESTAMP('2023-01-01'));
 
-CREATE TABLE IF NOT EXISTS node (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS node
+(
+    id          INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
     instance_id VARCHAR(255),
-    volume_id VARCHAR(255),
-    hostname VARCHAR(255),
-    vpc_id VARCHAR(255),
-    address VARCHAR(255) NOT NULL,
-    epoch INT NOT NULL DEFAULT 1,
-    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    volume_id   VARCHAR(255),
+    hostname    VARCHAR(255),
+    vpc_id      VARCHAR(255),
+    address     VARCHAR(255) NOT NULL,
+    epoch       INT          NOT NULL DEFAULT 1,
+    create_time DATETIME              DEFAULT CURRENT_TIMESTAMP,
+    update_time DATETIME              DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS topic (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    queue_num INT NOT NULL,
-    status tinyint DEFAULT 0,
+CREATE TABLE IF NOT EXISTS topic
+(
+    id          BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    queue_num   INT          NOT NULL,
+    status      tinyint  DEFAULT 0,
     create_time DATETIME DEFAULT current_timestamp,
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
-CREATE UNIQUE INDEX idx_topic_name ON topic(name);
+CREATE UNIQUE INDEX idx_topic_name ON topic (name);
 
-CREATE TABLE IF NOT EXISTS queue_assignment (
-    topic_id INT NOT NULL ,
-    queue_id INT NOT NULL ,
-    src_node_id INT NOT NULL ,
-    dst_node_id INT NOT NULL ,
-    status TINYINT NOT NULL ,
-    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE  CURRENT_TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS queue (
-    topic_id INT NOT NULL ,
-    queue_id INT NOT NULL ,
-    stream_id INT NOT NULL ,
-    role TINYINT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS consumer_group (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL ,
+CREATE TABLE IF NOT EXISTS queue_assignment
+(
+    topic_id    BIGINT  NOT NULL,
+    queue_id    INT     NOT NULL,
+    src_node_id INT     NOT NULL,
+    dst_node_id INT     NOT NULL,
+    status      TINYINT NOT NULL,
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS subscription (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    group_id INT NOT NULL ,
-    topic_id INT NOT NULL ,
-    expression VARCHAR(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS queue
+(
+    topic_id    BIGINT  NOT NULL,
+    queue_id    INT     NOT NULL,
+    stream_id   BIGINT  NOT NULL,
+    stream_role TINYINT NOT NULL DEFAULT 0
+);
+CREATE UNIQUE INDEX idx_queue_stream_id ON queue(stream_id);
+CREATE UNIQUE INDEX idx_queue ON queue(topic_id, queue_id, stream_role);
+
+CREATE TABLE IF NOT EXISTS consumer_group
+(
+    id          BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(255) NOT NULL,
+    status      TINYINT      NOT NULL DEFAULT 0,
+    create_time DATETIME              DEFAULT CURRENT_TIMESTAMP,
+    update_time DATETIME              DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+CREATE UNIQUE INDEX idx_consumer_group_name ON consumer_group (name);
+
+CREATE TABLE IF NOT EXISTS subscription
+(
+    id          BIGINT       NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    group_id    BIGINT       NOT NULL,
+    topic_id    BIGINT       NOT NULL,
+    expression  VARCHAR(255) NOT NULL,
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+CREATE UNIQUE INDEX idx_subscription_group_topic ON subscription (group_id, topic_id);
 
-CREATE TABLE IF NOT EXISTS stream (
-    stream_id BIGINT NOT NULL PRIMARY KEY,
-    epoch BIGINT NOT NULL,
-    range_id INT NOT NULL,
-    start_offset BIGINT NOT NULL,
-    state TINYINT NOT NULL
+CREATE TABLE IF NOT EXISTS group_progress
+(
+    group_id     BIGINT NOT NULL,
+    topic_id     BIGINT NOT NULL,
+    queue_id     INT    NOT NULL,
+    queue_offset BIGINT NOT NULL
+);
+CREATE UNIQUE INDEX idx_group_progress ON group_progress (group_id, topic_id, queue_id);
+
+CREATE TABLE IF NOT EXISTS stream
+(
+    stream_id    BIGINT  NOT NULL PRIMARY KEY,
+    epoch        BIGINT  NOT NULL,
+    range_id     INT     NOT NULL,
+    start_offset BIGINT  NOT NULL,
+    state        TINYINT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `range` (
-    range_id INT NOT NULL PRIMARY KEY,
-    stream_id BIGINT NOT NULL,
-    epoch BIGINT NOT NULL,
+CREATE TABLE IF NOT EXISTS `range`
+(
+    range_id     INT    NOT NULL PRIMARY KEY,
+    stream_id    BIGINT NOT NULL,
+    epoch        BIGINT NOT NULL,
     start_offset BIGINT NOT NULL,
-    end_offset BIGINT NOT NULL,
-    broker_id INT NOT NULL
+    end_offset   BIGINT NOT NULL,
+    broker_id    INT    NOT NULL
 );
 
 CREATE INDEX idx_range_stream_id ON `range` (stream_id);
 CREATE INDEX idx_range_broker_id ON `range` (broker_id);
 
-CREATE TABLE IF NOT EXISTS s3object (
-    object_id BIGINT NOT NULL PRIMARY KEY,
-    object_size BIGINT NOT NULL,
-    prepared_timestamp BIGINT ,
-    committed_timestamp BIGINT,
-    expired_timestamp BIGINT,
+CREATE TABLE IF NOT EXISTS s3object
+(
+    object_id                     BIGINT  NOT NULL PRIMARY KEY,
+    object_size                   BIGINT  NOT NULL,
+    prepared_timestamp            BIGINT,
+    committed_timestamp           BIGINT,
+    expired_timestamp             BIGINT,
     marked_for_deletion_timestamp BIGINT,
-    state TINYINT NOT NULL
+    state                         TINYINT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS s3streamobject (
-    stream_id BIGINT NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS s3streamobject
+(
+    stream_id    BIGINT NOT NULL PRIMARY KEY,
     start_offset BIGINT NOT NULL,
-    end_offset BIGINT NOT NULL,
-    object_id BIGINT NOT NULL
+    end_offset   BIGINT NOT NULL,
+    object_id    BIGINT NOT NULL
 );
 
 CREATE INDEX idx_s3streamobject_stream_id ON s3streamobject (stream_id);
 CREATE INDEX idx_s3streamobject_object_id ON s3streamobject (object_id);
 
-CREATE TABLE IF NOT EXISTS s3walobject (
-    sequence_id BIGINT NOT NULL PRIMARY KEY,
-    broker_id INT NOT NULL,
-    object_id BIGINT NOT NULL,
+CREATE TABLE IF NOT EXISTS s3walobject
+(
+    sequence_id  BIGINT NOT NULL PRIMARY KEY,
+    broker_id    INT    NOT NULL,
+    object_id    BIGINT NOT NULL,
     substream_id BIGINT NOT NULL
 );
 
@@ -129,11 +156,12 @@ CREATE INDEX idx_s3walobject_broker_id ON s3walobject (broker_id);
 CREATE INDEX idx_s3walobject_object_id ON s3walobject (object_id);
 CREATE INDEX idx_s3walobject_substream_id ON s3walobject (substream_id);
 
-CREATE TABLE IF NOT EXISTS substream (
+CREATE TABLE IF NOT EXISTS substream
+(
     substream_id BIGINT NOT NULL PRIMARY KEY,
-    stream_id BIGINT NOT NULL,
+    stream_id    BIGINT NOT NULL,
     start_offset BIGINT NOT NULL,
-    end_offset BIGINT NOT NULL
+    end_offset   BIGINT NOT NULL
 );
 
 CREATE INDEX idx_substream_stream_id ON substream (stream_id);
