6efbb4704a49ee44d03dab3cabd949683b9c411b
Shichao Nie <niesc@automq.com>
2023-10-17 21:28:24 +0800

    style(s3stream): fix checkstyle
    Signed-off-by: Shichao Nie <niesc@automq.com>

--- s3stream/src/test/java/com/automq/stream/s3/compact/CompactionAnalyzerTest.java
+++ s3stream/src/test/java/com/automq/stream/s3/compact/CompactionAnalyzerTest.java
@@ -32,7 +32,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
