74f4ff5146e0d13b9ddb1cc4e045c73006a13f25
TheR1sing3un <ther1sing3un@163.com>
2023-11-03 00:13:14 +0800

    feat(cli): support reset consume offset cli tool
    1. support reset consume offset cli tool
    
    Signed-off-by: TheR1sing3un <ther1sing3un@163.com>

--- cli/src/main/java/com/automq/rocketmq/cli/ResetConsumeOffset.java
+++ cli/src/main/java/com/automq/rocketmq/cli/ResetConsumeOffset.java
@@ -20,14 +20,19 @@ package com.automq.rocketmq.cli;
 import apache.rocketmq.controller.v1.Cluster;
 import apache.rocketmq.controller.v1.DescribeClusterRequest;
 import apache.rocketmq.controller.v1.MessageQueue;
+import apache.rocketmq.controller.v1.MessageQueueAssignment;
 import apache.rocketmq.controller.v1.Node;
 import apache.rocketmq.controller.v1.Topic;
+import apache.rocketmq.proxy.v1.ResetConsumeOffsetRequest;
 import com.automq.rocketmq.controller.client.GrpcControllerClient;
 import com.automq.rocketmq.proxy.grpc.client.GrpcProxyClient;
+import com.google.protobuf.TextFormat;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.tuple.Pair;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "resetConsumeOffset", description = "Reset consume offset", mixinStandardHelpOptions = true, showDefaultValues = true)
@@ -49,32 +54,64 @@ public class ResetConsumeOffset implements Callable<Void> {
 
     @Override
     public Void call() throws Exception {
+        // TODO: support retrying when failed because of cluster's state change at the same time
         GrpcControllerClient controllerClient = new GrpcControllerClient(new CliClientConfig());
         GrpcProxyClient proxyClient = new GrpcProxyClient(new CliClientConfig());
         CompletableFuture<Cluster> clusterCf = controllerClient.describeCluster(mqAdmin.endpoint, DescribeClusterRequest.newBuilder().build());
 
         CompletableFuture<Topic> topicCf = controllerClient.describeTopic(mqAdmin.endpoint, null, topicName);
-        clusterCf.thenCombine(topicCf, (cluster, topic) -> {
-            Map<Integer/*node id*/, Node> nodeMap = cluster.getNodesList().stream().collect(Collectors.toMap(node -> node.getId(), node -> node));
-            if (queueId == -1) {
-                // reset all queues
-                topic.getAssignmentsList().forEach(assignment -> {
-                    int nodeId = assignment.getNodeId();
-                    MessageQueue queue = assignment.getQueue();
-                    Node node = nodeMap.get(nodeId);
-                    if (node == null) {
-                        System.out.println("Node not found: " + nodeId);
-                        return;
-                    }
-
-                });
-
-            } else {
+        clusterCf.thenCombine(topicCf, Pair::of)
+            .thenComposeAsync(pair -> {
+                Cluster cluster = pair.getLeft();
+                Topic topic = pair.getRight();
+                Map<Integer/*node id*/, Node> nodeMap = cluster.getNodesList().stream().collect(Collectors.toMap(node -> node.getId(), node -> node));
+                if (queueId == -1) {
+                    // reset all queues
+                    CompletableFuture[] tqCfs = topic.getAssignmentsList().stream().map(assignment -> {
+                        int nodeId = assignment.getNodeId();
+                        MessageQueue queue = assignment.getQueue();
+                        Node node = nodeMap.get(nodeId);
+                        if (node == null) {
+                            System.out.println("Node not found: " + nodeId);
+                            return CompletableFuture.completedFuture(null);
+                        }
+                        String brokerAddr = node.getAddress();
+                        ResetConsumeOffsetRequest resetConsumeOffsetRequest = ResetConsumeOffsetRequest.newBuilder()
+                            .setTopic(topicName)
+                            .setQueueId(queue.getQueueId())
+                            .setGroup(consumerGroupName)
+                            .setNewConsumeOffset(newConsumeOffset)
+                            .build();
+                        return proxyClient.resetConsumeOffset(brokerAddr, resetConsumeOffsetRequest).thenAccept(nil -> {
+                            System.out.println("Reset consume offset success: brokerAddress: " + brokerAddr + " , " + TextFormat.shortDebugString(resetConsumeOffsetRequest));
+                        });
+                    }).toArray(CompletableFuture[]::new);
+                    return CompletableFuture.allOf(tqCfs);
+                }
                 // reset single queue
-
-            }
-            return null;
-        });
+                Optional<MessageQueueAssignment> queueAssignment =
+                    topic.getAssignmentsList().stream().filter(assignment -> assignment.getQueue().getQueueId() == queueId).findAny();
+                if (queueAssignment.isEmpty()) {
+                    System.out.println("Queue not found: " + queueId);
+                    return CompletableFuture.completedFuture(null);
+                }
+                int nodeId = queueAssignment.get().getNodeId();
+                Node node = nodeMap.get(nodeId);
+                if (node == null) {
+                    System.out.println("Node not found: " + nodeId);
+                    return CompletableFuture.completedFuture(null);
+                }
+                String brokerAddr = node.getAddress();
+                ResetConsumeOffsetRequest resetConsumeOffsetRequest = ResetConsumeOffsetRequest.newBuilder()
+                    .setTopic(topicName)
+                    .setQueueId(queueId)
+                    .setGroup(consumerGroupName)
+                    .setNewConsumeOffset(newConsumeOffset)
+                    .build();
+                return proxyClient.resetConsumeOffset(brokerAddr, resetConsumeOffsetRequest).thenAccept(nil -> {
+                    System.out.println("Reset consume offset success: brokerAddress: " + brokerAddr + " , " + TextFormat.shortDebugString(resetConsumeOffsetRequest));
+                });
+            }).join();
         return null;
     }
 }
