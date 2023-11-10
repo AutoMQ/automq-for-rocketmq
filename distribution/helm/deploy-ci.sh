274ea8eec2ed6eadc0f3587b2859dc1baeee84d0
Abyss <45425302+wangxye@users.noreply.github.com>
2023-11-10 10:05:00 +0800

    chore: optimize helm chart deployment (#611)
    Signed-off-by: wangxye <xuanyewang.cs@gmail.com>

--- distribution/helm/deploy-ci.sh
+++ distribution/helm/deploy-ci.sh
@@ -15,13 +15,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ROCKETMQ_REPO=$1
+ROCKETMQ_VERSION=$2
+NAMESPACE=$3
+
+if [[ -z $NAMESPACE ]]; then
+  NAMESPACE="default"
+  echo "info: NAMESPACE is empty, use default namespace: $NAMESPACE"
+fi
+
+echo "************************************"
+echo "*          init config...          *"
+echo "************************************"
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_DIR=$(dirname "$(dirname "$SCRIPT_DIR")")
-export $REPO_DIR
+echo "SCRIPT_DIR: $SCRIPT_DIR"
+echo "REPO_DIR: $REPO_DIR"
 cd "$SCRIPT_DIR" || exit 1
 if [ ! -f "deploy/ddl.sql" ]
 then
-  cp "$REPO_DIR/controller/src/main/resources/ddl.sql" deploy/
+  cp "$REPO_DIR/metadata-jdbc/src/main/resources/ddl.sql" deploy/
 fi
 
 # make ddl to configmap
@@ -31,23 +45,29 @@ then
   sed 's/^/    /' deploy/ddl.sql >> deploy/init-db-configmap.yaml
 fi
 
-kubectl apply -f deploy/init-db-configmap.yaml
+kubectl apply -f deploy/init-db-configmap.yaml --namespace $NAMESPACE
 
-echo "Deploying s3-localstack..."
+echo "************************************"
+echo "*     Create env and deploy...     *"
+echo "************************************"
+
+echo ${ROCKETMQ_REPO}:${ROCKETMQ_VERSION}: ${NAMESPACE} deploy start
+
 # deploy s3-localstack
 helm repo add localstack-charts https://localstack.github.io/helm-charts
-helm install s3-localstack localstack-charts/localstack -f deploy/localstack_s3.yaml
+helm install s3-localstack localstack-charts/localstack -f deploy/localstack_s3.yaml --namespace $NAMESPACE
 
-sleep 10
+# Wait for s3-localstack to be ready
+kubectl rollout status --watch --timeout=120s statefulset/s3-localstack --namespace $NAMESPACE
 
-echo "Deploying MySQL..."
 # deploy mysql
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install mysql bitnami/mysql -f deploy/mysql.yaml
+helm install mysql bitnami/mysql -f deploy/mysql.yaml --namespace $NAMESPACE
 
-sleep 10
-echo "Deploying rocketmq..."
+# Wait for mysql to be ready
+kubectl rollout status --watch --timeout=120s statefulset/mysql --namespace $NAMESPACE
+
 # deploy automq-for-rocketmq
-helm install automq-for-rocketmq . -f deploy/helm_sample_values.yaml
+helm install automq-for-rocketmq . -f deploy/helm_sample_values.yaml --set broker.image.repository=$ROCKETMQ_REPO --set broker.image.tag=$ROCKETMQ_VERSION --namespace $NAMESPACE
 
-
+kubectl rollout status --watch --timeout=360s statefulset/automq-for-rocketmq-broker --namespace $NAMESPACE
