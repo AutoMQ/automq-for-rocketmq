70bd7ea715596d4d5b363a7a6238454f5bba608d
SSpirits <admin@lv5.moe>
2023-11-16 14:23:50 +0800

    feat(proxy): add timeout for waiting for the future in ReceiveMessageActivity
    Signed-off-by: SSpirits <admin@lv5.moe>

--- proxy/src/main/java/com/automq/rocketmq/proxy/processor/ExtendMessagingProcessor.java
+++ proxy/src/main/java/com/automq/rocketmq/proxy/processor/ExtendMessagingProcessor.java
@@ -17,18 +17,45 @@
 
 package com.automq.rocketmq.proxy.processor;
 
+import apache.rocketmq.v2.Code;
+import com.automq.rocketmq.common.config.ProxyConfig;
+import com.automq.rocketmq.proxy.exception.ProxyException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import org.apache.rocketmq.broker.client.ConsumerManager;
 import org.apache.rocketmq.broker.client.ProducerManager;
+import org.apache.rocketmq.client.consumer.PopResult;
+import org.apache.rocketmq.proxy.common.ProxyContext;
 import org.apache.rocketmq.proxy.processor.DefaultMessagingProcessor;
+import org.apache.rocketmq.proxy.processor.PopMessageResultFilter;
+import org.apache.rocketmq.proxy.processor.QueueSelector;
 import org.apache.rocketmq.proxy.service.ServiceManager;
+import org.apache.rocketmq.remoting.protocol.heartbeat.SubscriptionData;
 
 public class ExtendMessagingProcessor extends DefaultMessagingProcessor {
-    protected ExtendMessagingProcessor(ServiceManager serviceManager) {
+    private final ProxyConfig proxyConfig;
+
+    protected ExtendMessagingProcessor(ServiceManager serviceManager, ProxyConfig proxyConfig) {
         super(serviceManager);
+        this.proxyConfig = proxyConfig;
     }
 
-    public static ExtendMessagingProcessor createForS3RocketMQ(ServiceManager serviceManager) {
-        return new ExtendMessagingProcessor(serviceManager);
+    public static ExtendMessagingProcessor createForS3RocketMQ(ServiceManager serviceManager, ProxyConfig proxyConfig) {
+        return new ExtendMessagingProcessor(serviceManager, proxyConfig);
+    }
+
+    @Override
+    public CompletableFuture<PopResult> popMessage(ProxyContext ctx, QueueSelector queueSelector, String consumerGroup,
+        String topic, int maxMsgNums, long invisibleTime, long pollTime, int initMode,
+        SubscriptionData subscriptionData, boolean fifo, PopMessageResultFilter popMessageResultFilter,
+        long timeoutMillis) {
+        long timeout = timeoutMillis - proxyConfig.networkRTTMills();
+        if (timeout < 0) {
+            return CompletableFuture.failedFuture(new ProxyException(Code.ILLEGAL_POLLING_TIME, "timeout is too small"));
+        }
+
+        return super.popMessage(ctx, queueSelector, consumerGroup, topic, maxMsgNums, invisibleTime, pollTime, initMode, subscriptionData, fifo, popMessageResultFilter, timeoutMillis)
+            .orTimeout(timeout, TimeUnit.MILLISECONDS);
     }
 
     public ServiceManager getServiceManager() {
