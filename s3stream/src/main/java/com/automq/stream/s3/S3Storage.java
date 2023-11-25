de97eba2cb279e65c155233082ac20269355df3f
Ning Yu <ningyu@automq.com>
2023-11-25 22:11:59 +0800

    fix: avoid overflow
    Signed-off-by: Ning Yu <ningyu@automq.com>

--- s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
+++ s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
@@ -413,7 +413,7 @@ public class S3Storage implements Storage {
     }
 
     private Lock getStreamCallbackLock(long streamId) {
-        return streamCallbackLocks[(int) (Math.abs(streamId) % NUM_STREAM_CALLBACK_LOCKS)];
+        return streamCallbackLocks[(int) ((streamId & Long.MAX_VALUE) % NUM_STREAM_CALLBACK_LOCKS)];
     }
 
     @SuppressWarnings("UnusedReturnValue")
