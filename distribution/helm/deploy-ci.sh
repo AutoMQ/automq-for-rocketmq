3afab4fadc2b6a8fded50945bb8d423fb4f879e7
Abyss <45425302+wangxye@users.noreply.github.com>
2023-12-07 09:55:32 +0800

    chore: fix helm chart install eof (#794)
    Signed-off-by: wangxye <xuanyewang.cs@gmail.com>

--- distribution/helm/deploy-ci.sh
+++ distribution/helm/deploy-ci.sh
@@ -53,26 +53,27 @@ echo "************************************"
 
 echo ${ROCKETMQ_REPO}:${ROCKETMQ_VERSION}: ${NAMESPACE} deploy start
 
-# deploy s3-localstack
+# add dependency repo
 helm repo add localstack-charts https://localstack.github.io/helm-charts
-helm install s3-localstack localstack-charts/localstack -f deploy/localstack_s3.yaml --namespace $NAMESPACE
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add automq http://charts.automq.com/
+helm repo update
+
+
+# deploy s3-localstack
+helm install s3-localstack localstack-charts/localstack --version 0.6.5 -f deploy/localstack_s3.yaml --namespace $NAMESPACE
 
 # Wait for s3-localstack to be ready
 kubectl rollout status --watch --timeout=120s replicaset/s3-localstack --namespace $NAMESPACE
 
 # deploy mysql
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install mysql bitnami/mysql -f deploy/mysql.yaml --namespace $NAMESPACE
+helm install mysql bitnami/mysql --version 9.14.1 -f deploy/mysql.yaml --namespace $NAMESPACE
 
 # Wait for mysql to be ready
 kubectl rollout status --watch --timeout=120s statefulset/mysql --namespace $NAMESPACE
 
-helm repo add automq http://charts.automq.com/
-
 # deploy automq-for-rocketmq
-helm install automq-for-rocketmq automq/automq-for-rocketmq -f deploy/helm_sample_values.yaml  \
-  --set broker.image.repository=$ROCKETMQ_REPO  \
-  --set broker.image.tag=$ROCKETMQ_VERSION  \
-  --namespace $NAMESPACE
+helm install automq-for-rocketmq automq/automq-for-rocketmq -f deploy/helm_sample_values.yaml --set broker.image.repository=$ROCKETMQ_REPO --set broker.image.tag=$ROCKETMQ_VERSION --namespace $NAMESPACE
 
+# Wait for automq-for-rocketmq to be ready
 kubectl rollout status --watch --timeout=360s statefulset/automq-for-rocketmq-rocketmq-broker --namespace $NAMESPACE
