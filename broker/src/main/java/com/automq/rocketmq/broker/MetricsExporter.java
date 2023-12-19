67b6bca5a487c3d521902630abcc453b61b9a591
SSpirits <admin@lv5.moe>
2023-12-19 11:59:24 +0800

    refactor(broker): refactor opentelemertry exporter
    Signed-off-by: SSpirits <admin@lv5.moe>

--- broker/src/main/java/com/automq/rocketmq/broker/MetricsExporter.java
+++ broker/src/main/java/com/automq/rocketmq/broker/MetricsExporter.java
@@ -29,41 +29,24 @@ import com.automq.rocketmq.store.MessageStoreImpl;
 import com.automq.rocketmq.store.metrics.StoreMetricsManager;
 import com.automq.rocketmq.store.metrics.StreamMetricsManager;
 import com.google.common.base.Splitter;
-import io.opentelemetry.api.baggage.propagation.W3CBaggagePropagator;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.common.AttributesBuilder;
 import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.context.propagation.TextMapPropagator;
-import io.opentelemetry.exporter.logging.LoggingMetricExporter;
-import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
-import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporterBuilder;
-import io.opentelemetry.exporter.prometheus.PrometheusHttpServer;
 import io.opentelemetry.instrumentation.oshi.SystemMetrics;
 import io.opentelemetry.instrumentation.runtimemetrics.java17.RuntimeMetrics;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.OpenTelemetrySdkBuilder;
 import io.opentelemetry.sdk.metrics.InstrumentSelector;
-import io.opentelemetry.sdk.metrics.InstrumentType;
-import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.SdkMeterProviderBuilder;
 import io.opentelemetry.sdk.metrics.View;
-import io.opentelemetry.sdk.metrics.data.AggregationTemporality;
-import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.Optional;
 import java.util.function.Supplier;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.rocketmq.common.metrics.MetricsExporterType;
 import org.apache.rocketmq.logging.org.slf4j.Logger;
 import org.apache.rocketmq.logging.org.slf4j.LoggerFactory;
-import org.slf4j.bridge.SLF4JBridgeHandler;
 
 import static com.automq.rocketmq.broker.MetricsConstant.AGGREGATION_CUMULATIVE;
 import static com.automq.rocketmq.broker.MetricsConstant.AGGREGATION_DELTA;
@@ -73,16 +56,12 @@ import static com.automq.rocketmq.broker.MetricsConstant.LABEL_NODE_NAME;
 
 public class MetricsExporter implements Lifecycle {
     private static final Logger LOGGER = LoggerFactory.getLogger(MetricsExporter.class);
+    private static final Map<String, String> LABEL_MAP = new HashMap<>();
     private volatile boolean started = false;
     private final BrokerConfig brokerConfig;
     private final MetricsConfig metricsConfig;
-    private final static Map<String, String> LABEL_MAP = new HashMap<>();
-    private OtlpGrpcMetricExporter metricExporter;
-    private PeriodicMetricReader periodicMetricReader;
-    private PrometheusHttpServer prometheusHttpServer;
-    private LoggingMetricExporter loggingMetricExporter;
+    private final TelemetryExporter telemetryExporter;
     private Meter brokerMeter;
-    private OpenTelemetrySdk openTelemetrySdk;
     private RuntimeMetrics runtimeMetrics;
 
     private final ProxyMetricsManager proxyMetricsManager;
@@ -93,16 +72,17 @@ public class MetricsExporter implements Lifecycle {
 
     public static Supplier<AttributesBuilder> attributesBuilderSupplier = Attributes::builder;
 
-    public MetricsExporter(BrokerConfig brokerConfig, MessageStoreImpl messageStore,
-        ExtendMessagingProcessor messagingProcessor, Resource resource, SdkTracerProvider tracerProvider,
-        MetadataStore metadataStore, S3MetadataService s3MetadataService) {
+    public MetricsExporter(BrokerConfig brokerConfig, TelemetryExporter telemetryExporter,
+        MessageStoreImpl messageStore, ExtendMessagingProcessor messagingProcessor, MetadataStore metadataStore,
+        S3MetadataService s3MetadataService) {
         this.brokerConfig = brokerConfig;
         this.metricsConfig = brokerConfig.metrics();
+        this.telemetryExporter = telemetryExporter;
         this.proxyMetricsManager = new ProxyMetricsManager(messagingProcessor);
         this.storeMetricsManager = new StoreMetricsManager(metricsConfig, messageStore);
         this.streamMetricsManager = new StreamMetricsManager();
         this.topicMetricsManager = new TopicMetricsManager(metadataStore, s3MetadataService);
-        init(resource, tracerProvider);
+        init();
     }
 
     public static AttributesBuilder newAttributesBuilder() {
@@ -115,30 +95,9 @@ public class MetricsExporter implements Lifecycle {
         return attributesBuilder;
     }
 
-    private boolean checkConfig() {
-        if (metricsConfig == null) {
-            return false;
-        }
-        MetricsExporterType exporterType = MetricsExporterType.valueOf(metricsConfig.exporterType());
-        if (!exporterType.isEnable()) {
-            return false;
-        }
-
-        return switch (exporterType) {
-            case OTLP_GRPC -> StringUtils.isNotBlank(metricsConfig.grpcExporterTarget());
-            case PROM, LOG -> true;
-            default -> false;
-        };
-    }
-
-    private void init(Resource resource, SdkTracerProvider tracerProvider) {
-        MetricsExporterType metricsExporterType = MetricsExporterType.valueOf(metricsConfig.exporterType());
-        if (metricsExporterType == MetricsExporterType.DISABLE) {
-            return;
-        }
-
-        if (!checkConfig()) {
-            LOGGER.error("check metrics config failed, will not export metrics");
+    private void init() {
+        if (!telemetryExporter.isMetricsEnabled()) {
+            LOGGER.info("MetricsExporter is disabled");
             return;
         }
 
@@ -163,81 +122,13 @@ public class MetricsExporter implements Lifecycle {
         LABEL_MAP.put(LABEL_NODE_NAME, brokerConfig.name());
         LABEL_MAP.put(LABEL_INSTANCE_ID, brokerConfig.instanceId());
 
-        SdkMeterProviderBuilder providerBuilder = SdkMeterProvider.builder()
-            .setResource(resource);
-
-        if (metricsExporterType == MetricsExporterType.OTLP_GRPC) {
-            String endpoint = metricsConfig.grpcExporterTarget();
-            if (!endpoint.startsWith("http")) {
-                endpoint = "https://" + endpoint;
-            }
-            OtlpGrpcMetricExporterBuilder metricExporterBuilder = OtlpGrpcMetricExporter.builder()
-                .setEndpoint(endpoint)
-                .setTimeout(metricsConfig.grpcExporterTimeOutInMills(), TimeUnit.MILLISECONDS)
-                .setAggregationTemporalitySelector(type -> {
-                    if (metricsConfig.exportInDelta() &&
-                        (type == InstrumentType.COUNTER || type == InstrumentType.OBSERVABLE_COUNTER || type == InstrumentType.HISTOGRAM)) {
-                        return AggregationTemporality.DELTA;
-                    }
-                    return AggregationTemporality.CUMULATIVE;
-                });
-
-            String headers = metricsConfig.grpcExporterHeader();
-            if (StringUtils.isNotBlank(headers)) {
-                Map<String, String> headerMap = new HashMap<>();
-                List<String> kvPairs = Splitter.on(',').omitEmptyStrings().splitToList(headers);
-                for (String item : kvPairs) {
-                    String[] split = item.split(":");
-                    if (split.length != 2) {
-                        LOGGER.warn("metricsGrpcExporterHeader is not valid: {}", headers);
-                        continue;
-                    }
-                    headerMap.put(split[0], split[1]);
-                }
-                headerMap.forEach(metricExporterBuilder::addHeader);
-            }
-
-            metricExporter = metricExporterBuilder.build();
-
-            periodicMetricReader = PeriodicMetricReader.builder(metricExporter)
-                .setInterval(metricsConfig.periodicExporterIntervalInMills(), TimeUnit.MILLISECONDS)
-                .build();
-
-            providerBuilder.registerMetricReader(periodicMetricReader);
-        } else if (metricsExporterType == MetricsExporterType.PROM) {
-            String promExporterHost = metricsConfig.promExporterHost();
-            if (StringUtils.isBlank(promExporterHost)) {
-                throw new IllegalArgumentException("Config item promExporterHost is blank");
-            }
-            prometheusHttpServer = PrometheusHttpServer.builder()
-                .setHost(promExporterHost)
-                .setPort(metricsConfig.promExporterPort())
-                .build();
-            providerBuilder.registerMetricReader(prometheusHttpServer);
-        } else if (metricsExporterType == MetricsExporterType.LOG) {
-            SLF4JBridgeHandler.removeHandlersForRootLogger();
-            SLF4JBridgeHandler.install();
-            loggingMetricExporter = LoggingMetricExporter.create(metricsConfig.exportInDelta() ? AggregationTemporality.DELTA : AggregationTemporality.CUMULATIVE);
-            java.util.logging.Logger.getLogger(LoggingMetricExporter.class.getName()).setLevel(java.util.logging.Level.FINEST);
-            periodicMetricReader = PeriodicMetricReader.builder(loggingMetricExporter)
-                .setInterval(metricsConfig.periodicExporterIntervalInMills(), TimeUnit.MILLISECONDS)
-                .build();
-            providerBuilder.registerMetricReader(periodicMetricReader);
+        Optional<OpenTelemetrySdk> optional = telemetryExporter.openTelemetrySdk();
+        if (optional.isEmpty()) {
+            LOGGER.warn("OpenTelemetrySdk is not initialized");
+            return;
         }
 
-        registerMetricsView(providerBuilder);
-
-        OpenTelemetrySdkBuilder telemetrySdkBuilder = OpenTelemetrySdk.builder();
-
-        if (tracerProvider != null) {
-            telemetrySdkBuilder.setTracerProvider(tracerProvider);
-        }
-
-        openTelemetrySdk = telemetrySdkBuilder
-            .setPropagators(ContextPropagators.create(TextMapPropagator.composite(W3CTraceContextPropagator.getInstance(), W3CBaggagePropagator.getInstance())))
-            .setMeterProvider(providerBuilder.build())
-            .buildAndRegisterGlobal();
-
+        OpenTelemetrySdk openTelemetrySdk = optional.get();
         brokerMeter = openTelemetrySdk.getMeter("automq-for-rocketmq");
 
         // JVM metrics
@@ -281,14 +172,13 @@ public class MetricsExporter implements Lifecycle {
 
     @Override
     public void start() {
-        MetricsExporterType metricsExporterType = MetricsExporterType.valueOf(metricsConfig.exporterType());
-        if (metricsExporterType != MetricsExporterType.DISABLE) {
+        if (telemetryExporter.isMetricsEnabled()) {
             initDynamicMetrics();
             this.started = true;
         }
     }
 
-    private void registerMetricsView(SdkMeterProviderBuilder providerBuilder) {
+    protected static void registerMetricsView(SdkMeterProviderBuilder providerBuilder) {
         for (Pair<InstrumentSelector, View> selectorViewPair : ProxyMetricsManager.getMetricsView()) {
             providerBuilder.registerView(selectorViewPair.getLeft(), selectorViewPair.getRight());
         }
@@ -307,24 +197,10 @@ public class MetricsExporter implements Lifecycle {
         if (!started) {
             return;
         }
-        MetricsExporterType exporterType = MetricsExporterType.valueOf(metricsConfig.exporterType());
-        if (exporterType == MetricsExporterType.OTLP_GRPC) {
-            periodicMetricReader.forceFlush();
-            periodicMetricReader.shutdown();
-            metricExporter.shutdown();
-        } else if (exporterType == MetricsExporterType.PROM) {
-            prometheusHttpServer.forceFlush();
-            prometheusHttpServer.shutdown();
-        } else if (exporterType == MetricsExporterType.LOG) {
-            periodicMetricReader.forceFlush();
-            periodicMetricReader.shutdown();
-            loggingMetricExporter.shutdown();
-        }
         if (runtimeMetrics != null) {
             runtimeMetrics.close();
         }
         storeMetricsManager.shutdown();
-        openTelemetrySdk.shutdown();
     }
 }
 
