dba00905cef91b63ae26b4577566d8c417e56d2d
SSpirits <admin@lv5.moe>
2024-01-03 14:13:07 +0800

    feat(s3stream): wait for the background executor to close when shutting down the S3 storage
    Signed-off-by: SSpirits <admin@lv5.moe>

--- s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
+++ s3stream/src/main/java/com/automq/stream/s3/S3Storage.java
@@ -258,6 +258,13 @@ public class S3Storage implements Storage {
         }
         deltaWAL.shutdownGracefully();
         backgroundExecutor.shutdown();
+        try {
+            if (backgroundExecutor.awaitTermination(10, TimeUnit.SECONDS)) {
+                LOGGER.warn("await backgroundExecutor timeout 10s");
+            }
+        } catch (InterruptedException e) {
+            LOGGER.warn("await backgroundExecutor close fail", e);
+        }
     }
 
     @Override
