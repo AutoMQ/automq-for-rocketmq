dcefe5a792eb4165a8bcd5deb74e0a6db41e1cf4
daniel-y <130526409+daniel-y@users.noreply.github.com>
2024-02-02 16:21:27 +0800

    chore: convert license to bsl to accelerate open source innovation (#913)
    Signed-off-by: daniel-y <daniel@automq.com>

--- metadata-jdbc/src/main/resources/ddl.sql
+++ metadata-jdbc/src/main/resources/ddl.sql
@@ -1,4 +1,15 @@
 /*
+ * Copyright 2024, AutoMQ CO.,LTD.
+ *
+ * Use of this software is governed by the Business Source License
+ * included in the file BSL.md
+ *
+ * As of the Change Date specified in that file, in accordance with
+ * the Business Source License, use of this software will be governed
+ * by the Apache License, Version 2.0
+ */
+
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
