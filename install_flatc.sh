cec0979adb224b3e49253a43dfebf5669c11dd07
daniel-y <daniel@automq.com>
2024-02-02 15:58:37 +0800

    chore: convert license to bsl to accelerate open source innovation
    Signed-off-by: daniel-y <daniel@automq.com>

--- install_flatc.sh
+++ install_flatc.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+# Copyright 2024, AutoMQ CO.,LTD.
+#
+# Use of this software is governed by the Business Source License
+# included in the file BSL.md
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0
+#
+
 set -e
 
 if [ "$EUID" -ne 0 ]
